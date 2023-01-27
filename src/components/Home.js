cad2e908499e2e437a3260c80d65ed38484897b3
pallav-data <pallavraj5247747@gmail.com>
2023-01-27 14:02:36 +0530

    added news error

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,23 +9,26 @@ const Home = () =>{
         let data = await fetch(url);
         let jsonData = await data.json()
         setArticles(jsonData.articles)
-        console.log(jsonData)
+        console.log("jsondata ".concat(jsonData.articles))
     }
     useEffect(() => {
         newsData(); 
     }, [])
+
     return (
         <>
         <Nav/>
+        {articles.length?(
             <div className="container-fluid back-dark">
             <div className="row">
-                {articles?.map((element) => {
+                {articles.map((element) => {
                     return <div className="col-lg-4 col-md-6" key={element.url}>
                         <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                     </div>
                 })}
             </div>
             </div>
+    ):<h1 className="text-center mt-4">Free version "newsapi" only works on local host</h1>}
     </>
 
     );
