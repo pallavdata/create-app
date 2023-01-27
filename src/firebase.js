21ea978e06c0c2f43f7b7062f7588446ac2b13c0
pallav-data <pallavraj5247747@gmail.com>
2023-01-27 11:48:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey:"",
+  apiKey: "AIzaSyBlcyJkJv3frakZo4ZwsIIqC5ed5LoM1t0",
   authDomain: "react-pallav.firebaseapp.com",
   projectId: "react-pallav",
   storageBucket: "react-pallav.appspot.com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  messagingSenderId: "1032604083092",
+  appId: "1:1032604083092:web:04493a2e5e374ff0c5e581",
+  measurementId: "G-T5N3MJQK0P"
 };
 
 // Initialize Firebase
